0bd755be44fdbcca35ac1339bf0fd9814a2e39f9
sarah <sarah.teoh752@gmail.com>
2021-06-15 14:35:05 +0900

    Add log in page

--- login.php
+++ login.php
@@ -4,22 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registration</title>
+    <title>Log in</title>
 </head>
 <body>
     <div class="header">
-        <h2>Register</h2>
+        <h2>Log in</h2>
     </div>
 
-    <form action="registration.php" method="post">
+    <form action="login.php" method="post">
         <div>
             <label for="username">Username : </label>
             <input type="text" name="username" required>
-        </div>
-
-        <div>
-            <label for="email">Email : </label>
-            <input type="text" name="email" required>
         </div>
 
         <div>
@@ -27,14 +22,9 @@
             <input type="text" name="password" required>
         </div>
 
-        <div>
-            <label for="confirm-password">Confirm Password : </label>
-            <input type="text" name="confirm-password" required>
-        </div>
+        <button type="submit" name="login" value="login">Log in</button>
 
-        <button type="submit" name="register" value="register">Submit</button>
-
-        <p>Already a user? <a href="login.php">Log in</a></p>
+        <p>Not a user? <a href="registration.php">Register here</a></p>
     </form>
 
     
